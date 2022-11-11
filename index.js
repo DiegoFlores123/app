c0013cc90e1d17f80436b4b1e157f1bce30fe519
DiegoFlores123 <dflores30@ucol.mx>
2022-11-10 20:09:28 -0600

    Version 1.5

---
+++ index.js
@@ -0,0 +1,11 @@
+let variable = "Hola mundo!";
+let num1 = parseInt(prompt("Dame un valor"))
+let num2 = parseInt(prompt("Dame otro valor"))
+
+function suma(num1, num2){
+    return num1 + num2
+}
+
+alert(suma(num1, num2))
+alert(variable)
+console.log("Prueba en consola")
